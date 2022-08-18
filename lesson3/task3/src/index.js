3cd4e8bab0a601e5e6361033aeb67dcd8888b2a7
JulietShaban <julietshaban@gmail.com>
2022-08-18 15:24:30 +0300

    class components lesson

--- lesson3/task3/src/index.js
+++ lesson3/task3/src/index.js
@@ -1,25 +1,19 @@
-
-
-
-
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const style ={
-  color: 'red', 
-  fontWeight: 700
+class A extends Component {
+  render() {
+    return (
+      <div class="search">
+        <h1 class="search__title">{`Hello, ${this.props.name} What to search for you?`}</h1>
+        <div class="search__field">
+          <input type="text" class="search__input" />
+          <button class="search__button">Search</button>
+        </div>
+      </div>
+    );
+  }
 }
-
-const element = (
-  <div class="search">
-  <h1 class="search__title">Hello, Tom. What to search for you?</h1>
-  <div class="search__field">
-      <input type="text" class="search__input" />
-      <button class="search__button">Search</button>
-  </div>
-</div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>{element}</React.StrictMode>);
+root.render(<React.StrictMode>{<A name="Bob" />}</React.StrictMode>);
